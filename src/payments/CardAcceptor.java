61b49374055d5b0933b5d8a952f1ea8f950852a7
Kamilla Osmonova <kameakadze@gmail.com>
2024-04-25 16:05:18 +0600

    create methods for password , number of card with checking to any exceptions

--- src/payments/CardAcceptor.java
+++ src/payments/CardAcceptor.java
@@ -1,7 +1,14 @@
 package payments;
 
-public class CardAcceptor implements MoneyPayment{
+import exceptions.CatchPaymentException;
+
+import java.util.Scanner;
+
+public class CardAcceptor implements MoneyPayment {
     private int amount;
+    private static int balance = 500;
+    private String numberOfCard;
+    private String password;
 
     public CardAcceptor(int amount) {
         this.amount = amount;
@@ -20,5 +27,48 @@ public class CardAcceptor implements MoneyPayment{
     @Override
     public void addMoney() {
 
+    }
+
+    public static String inputFromConsole(String text) {
+        while (true) {
+            System.out.println(text);
+            String str = new Scanner(System.in).nextLine();
+            if (str.isEmpty() || str.isBlank()) {
+                System.err.println("Не корректно введены данные.");
+            } else {
+                return str;
+            }
+        }
+    }
+    public static String tryParseToNumber(String text) {
+        while (true) {
+            String resultText = inputFromConsole(text);
+            try {
+                long result = Long.parseLong(resultText);
+                if (result < 0) {
+                    throw new CatchPaymentException("Пароль должен содержать только цифры.");
+                }
+                return resultText;
+            } catch (NumberFormatException | CatchPaymentException e) {
+                e.printStackTrace();
+                System.err.println("Введены неверные данные.");
+            }
+        }
+    }
+
+    public static int parseInt(String text) {
+        while (true) {
+            String resultText = inputFromConsole(text);
+            try {
+                int result = Integer.parseInt(resultText);
+                if (result < 0) {
+                    throw new CatchPaymentException("Введенное значение не должно быть меньше 0.");
+                }
+                return result;
+            } catch (NumberFormatException | CatchPaymentException e) {
+                e.printStackTrace();
+                System.err.println("Введены неверные данные.");
+            }
+        }
     }
 }
